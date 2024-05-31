911dbd3f54195cc2afd802242a02eb7542dbd5fd
Mickael <jametmickael1@gmail.com>
2024-05-31 09:47:50 +0200

    modif countSymptom + debut javadoc

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -15,35 +15,24 @@ public class AnalyticsCounter {
 	}
 	
 	public List<String> getSymptoms() {
-		return symptomReader.GetSymptoms();
+		return symptomReader.getSymptoms();
 	}
 	
 	public Map<String, Integer> countSymptoms(List<String> symptoms) {
-		Map<String, Integer> list = new HashMap<String, Integer>();
+		Map<String, Integer> symptomsList = new HashMap<String, Integer>();
 		for (String symptom : symptoms) {
-			if (list.keySet().isEmpty()) {
-				list.put(symptom, 1);
-				System.out.println("vide");
-			}
-			else {
-				for (String string : list.keySet()) {
-					System.out.println(symptom + " : " + string);
-					if (symptom == string) {
-						System.out.println("+1");
-						list.put(symptom, list.get(symptom) + 1);
-					}
-				}
+			if (symptomsList.putIfAbsent(symptom, 1) != null) {
+				symptomsList.put(symptom, symptomsList.get(symptom) + 1);
 			}
 		}
-		System.out.println(list);
 		
-		return list;
+		return symptomsList;
 	}
 	
 	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
-		TreeMap<String, Integer> sorted = new TreeMap<>();
-		sorted.putAll(symptoms);
-		return sorted;
+		TreeMap<String, Integer> sortMap = new TreeMap<>();
+		sortMap.putAll(symptoms);
+		return sortMap;
 	}
 	
 	public void writeSymptoms(Map<String, Integer> symptoms) {
